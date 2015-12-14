0e2abd9718c0fac91e9880e84bafd45a077b47b2
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2015-12-14 12:13:08 +0000

    bugfix (ht: @billy322)

--- evalrank.py
+++ evalrank.py
@@ -32,7 +32,7 @@ def argparser():
     return ap
 
 def cosine(v1, v2):
-    return numpy.dot(v1/numpy.linalg.norm(v1), v2/numpy.linalg.norm(v1))
+    return numpy.dot(v1/numpy.linalg.norm(v1), v2/numpy.linalg.norm(v2))
 
 def dot(v1, v2):
     return numpy.dot(v1, v2)
